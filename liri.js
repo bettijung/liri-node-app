5276a7a2cb31dece522435aaabacfb3df2957d51
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-02-13 18:04:29 -0500

    some liri set up code

--- liri.js
+++ liri.js
@@ -8,20 +8,91 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
-// This file can take in one of the following commands
-	// `my-tweets`
-		//This will show your last 20 tweets and when they were created at in your terminal window
-		node liri.js my-tweets 
+// Take 2 arguments for the user's input actions
+var action = process.argv[2];
+var name = process.argv[3];
 
-	// `spotify-this-song`
-		// This will show the following information about the song in your terminal window:
+
+// Switch-case to direct which function gets run
+switch(action) {
+	
+	case "my-tweets":
+		myTweets();
+		break;
+	
+	case "spotify-this-song":
+		spotifyThisSong();
+		break;
+	
+	case "movie-this":
+		movieThis();
+		break;
+	
+	case "do-what-it-says":
+		doWhatItSays();
+		break;		
+}
+
+
+// Commands Available
+	// myTweets: will show last 20 tweets and when they were created
+		function myTweets() {
+			console.log("hi");
+		}
+
+	// spotifyThisSong: will show song information	
+		function spotifyThisSong() {
+
 			// Artist(s)
 			// Song name
 			// Preview link of the song from Spotify
 			// The album that he song is from
-		node liri.js spotify-this-song '<I Want it That Way>'
-		// If no song is provided then program will default to "The Sign" by Ace of Base
 
-	// `movie-this`
+		// If no song is provided, default to "The Sign" by Ace of Base
+		}
 
-	// `do-what-it-says`
+
+	// movieThis: will output movie information
+		function movieThis() {
+			
+				// Title of the movie
+			// Year the movie came out
+			// IMDB Rating of the movie
+			// Rotten Tomatoes Rating of the movie
+			// Country where the movie was produced
+			// Language of the movie
+			// Plot of the movie
+			// Actors in the movie
+
+		// If no movie name is provided, the program will output fata for hte movie 'Mr.Nobody'
+		}
+
+
+	// doWhatItSays: text in random.txt will call one of liri's commands
+		// Using the fs Node package, liri will take the text inside of random.txt and use it to call one of liri's commands
+			// It should run spotify-this-song for "I Want it That Way" as follows the text in random.txt
+		function doWhatItSays() {
+
+		}
+
+
+	// In addition to logging the data to terminal window, output the data to log.txt
+	// Make sure you append each command you run to the log.txt file
+	// Make sure not to overwrite file each time you run a comment
+
+
+
+// Add README.md to repository
+
+
+
+
+
+
+
+
+
+
+
+
+
